35b6066bfb78cf0cb71a448a265ce0bffa434ca6
abdulla <rakib0751@gmail.com>
2021-09-22 02:29:55 +0200

    first_c#_push_in_git

---
+++ Rakibfirst1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+
+namespace Rakibfirst1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //practic the primitiveTypes
+          
+            string answer = String.Empty;
+            string englishMark= (Console.ReadLine());
+            double engMark;
+
+            while (!double.TryParse(englishMark, out engMark) || (engMark > 100) || (engMark < 0)) {
+
+                answer=($"you entered the Invalid value! {englishMark}");
+                Console.WriteLine(answer);
+                englishMark = (Console.ReadLine());
+              
+            }
+
+            if (double.TryParse(englishMark, out engMark)) {
+
+                if (engMark > 50 && engMark <= 100)
+                {
+
+                    answer=($"Your english mark is {engMark}" +
+                        $" and you successfully passed the exam!");
+              
+                }
+                else
+                {
+                    answer=("you can try again sorry my dear " +
+                        $"your mark is not sufficient only {engMark} out of 100 !!");
+                   
+
+                }
+              
+
+            }
+            else
+            {
+                answer=("you entered the Invalid value!");
+                
+            }
+
+            Console.WriteLine(answer);
+            Console.ReadKey();
+
+        }
+
+    }
+}
+ 
